37eaf5b1f1e1b0efb5a5b83a0f248218812dca5b
sdub18 <sdubois@umass.edu>
2022-03-24 23:23:23 -0400

    commented out the alembic stuff for right now. realized it was using MySQL but we use PostgreSQL

--- startup.sh
+++ startup.sh
@@ -2,12 +2,12 @@
 
 # Is the database read to accept connections?
 # If the database is not ready to accept connections and this script proceeds regardless, the Docker container will crash.
-while ! mysqladmin ping -h"database" --silent; do
-  sleep 1
-done
+# while [pg_isready -h db -p 5432 == ] do
+#   sleep 1
+# done
 
-# Run all migrations on the database.
-alembic upgrade head
+# # Run all migrations on the database.
+# alembic upgrade head
 
 # start the server
 uvicorn app.main:app --host 0.0.0.0 --port 8000
